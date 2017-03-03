7d925e19506e559c7fbfa1d0c691a58d4b7b80e1
Jaap <wijnen.jaap@gmail.com>
2017-03-03 01:09:15 +0100

    first commits

---
+++ Gimma/Gimma/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Gimma
+//
+//  Created by Jaap Wijnen on 03/03/2017.
+//  Copyright (c) 2017 Workmoose. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
