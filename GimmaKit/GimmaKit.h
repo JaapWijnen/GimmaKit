5600451d7257b2fa8afe9ee7ff366f002e482e36
Jaap <wijnen.jaap@gmail.com>
2017-03-03 01:00:50 +0100

    initial commit

---
+++ GimmaKit/GimmaKit.h
@@ -0,0 +1,19 @@
+//
+//  GimmaKit.h
+//  GimmaKit
+//
+//  Created by Jaap Wijnen on 03/03/2017.
+//  Copyright © 2017 Workmoose. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GimmaKit.
+FOUNDATION_EXPORT double GimmaKitVersionNumber;
+
+//! Project version string for GimmaKit.
+FOUNDATION_EXPORT const unsigned char GimmaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GimmaKit/PublicHeader.h>
+
+
